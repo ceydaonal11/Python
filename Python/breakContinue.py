8681b71fa90466fb11bf823af5f022ed16750482
Ceyda Önal <66831721+ceydaonal11@users.noreply.github.com>
2022-06-02 11:49:06 +0300

    Add files via upload

---
+++ Python/breakContinue.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+sayac=0
+while(sayac<10):
+    print(sayac)
+    if(sayac==5):
+        break
+    
+    sayac +=1
+    
+print("")   
+sayac=0
+while(sayac<10):
+    
+    if(sayac==5):
+        sayac +=1
+        continue
+    print(sayac)
+    sayac +=1
---
+++ Python/ifOrnekleri.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+a=4
+b=5
+c=6
+"""
+if(a==3):
+    print(a)
+    print(b)
+    
+print("Merhaba")
+
+
+yas=int(input("Yaşınızı giriniz: "))
+
+if(yas >= 20):
+    print("Dışarı çıkabilirsiniz")
+    
+else:
+    print("Dışarı çıkamazsınız")
+    
+"""
+
+
+sayi=int(input("Lütfen bir sayı giriniz:"))
+
+if(sayi >= 0):
+    if(sayi % 2==0):
+        print("Girilen sayı çifttir.")
+    else:
+        print("Girilen sayı tektir.")
+else:
+    print("Sayı negatiftir")
+    
+    
+    
+    
+    
+    
+    
+    
+    
