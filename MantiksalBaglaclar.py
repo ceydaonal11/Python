64269db7c53838f5d5e869dd04dfaecd74e4463d
Ceyda Önal <66831721+ceydaonal11@users.noreply.github.com>
2022-06-02 11:50:14 +0300

    Add files via upload

---
+++ MantiksalBaglaclar.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+# and, or
+
+
+#and-> her iki durumunda dogru olması sonucu dogru yapar
+#or-> herhangi bir durumun dogru olması sonucu dogru yapar
+
+
+print(1<3)
+
+
+print(1<3 and 4<3)
+
+print(True and True)
+print(False and True)
+print(True and False)
+print(False and False)
+
+print(True or True)
+print(False or True)
+print(True or False)
+print(False or False)
+
+
+print(2==2 and 4.5<2.6 and "asd" != "wert")
+
+print( not True)
+
+print( not (4==4))
+
+
+print(2==2 or 4.5<2.6 or "asd" != "wert")
+
+
+print(2==2 and 4.5<2.6 or "asd" == "wert")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ whileOrnekleri.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# sayac=0
+
+# while(sayac<10):
+#     print(sayac)
+#     sayac +=2
+    
+# sayac=0
+
+# while(sayac<10):
+#     print("Merhaba Dünya")
+#     sayac +=1
+    
+    
+#sonsuz dongu
+# sayac=0   
+# while(sayac<10):
+#     print(sayac)
+    
+
+while(True):
+    print("Sonsuz")
+ 
